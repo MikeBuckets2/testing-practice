3b993aff727999aa0b569f20310a094a4d1a0f2a
Michael Morris <michaelmorris121@yahoo.com>
2026-01-30 19:12:33 -0600

    create test to for basic operations

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseStr } from './script.js';
+import { capitalize, reverseStr, calculator } from './script.js';
 
 test('capitalize first letter of string', () => {
     expect(capitalize('word')).toBe('Word');
@@ -6,4 +6,20 @@ test('capitalize first letter of string', () => {
 
 test('takes a string and reverse it', () => {
     expect(reverseStr('tan')).toBe('nat');
+});
+
+test('adds two numbers', () => {
+    expect(calculator.add(2, 3)).toBe(5);
+});
+
+test('subtracts two numbers', () => {
+    expect(calculator.subtract(5, 2)).toBe(3);
+});
+
+test('multiplies two numbers', () => {
+    expect(calculator.multiply(4, 3)).toBe(12);
+});
+
+test('divides two numbers', () => {
+    expect(calculator.divide(10, 2)).toBe(5);
 });
